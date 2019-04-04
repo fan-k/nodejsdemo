6ef4a49067b66477da6e6adaa0e60a893dffc5e5
fank <15936219505@163.com>
2019-04-04 16:59:23 +0800

    Update users.js

--- apitest/app/models/users.js
+++ apitest/app/models/users.js
@@ -3,35 +3,35 @@
  * 
  */
 
- const mysql = require('mysql')
+//  const mysql = require('mysql')
 
- var lianjie = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Mysql123..',
-    port:'3306',
-    database:'mysqltest',
- })
- lianjie.connect()
+//  var lianjie = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Mysql123..',
+//     port:'3306',
+//     database:'mysqltest',
+//  })
+//  lianjie.connect()
 
- var sql = 'SELECT * FROM prize'
+//  var sql = 'SELECT * FROM prize'
 
- lianjie.query(sql,function(error,result){
-     if(error)console.log(error)
-     else
-        console.log(result)
- })
+//  lianjie.query(sql,function(error,result){
+//      if(error)console.log(error)
+//      else
+//         console.log(result)
+//  })
 
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// const userSchema = new Schema({
-//     user_id :Number,
-//     user_name :String,
-// })
-// console.log('userschema'+ userSchema)
-// //建立索引 提高查询效率
-// userSchema.index({user_id:1})
-// const userinfo = mongoose.model('userinfo',userSchema)
+const userSchema = new Schema({
+    user_id :Number,
+    user_name :String,
+})
+console.log('userschema'+ userSchema)
+//建立索引 提高查询效率
+userSchema.index({user_id:1})
+const userinfo = mongoose.model('userinfo',userSchema)
 
-// module.exports = userinfo
+module.exports = userinfo
